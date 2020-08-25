b36cc254178ecc5e1f8b4f688bededeaab58da09
wuxiaoran <wuxiaoran@xinzhili.cn>
2020-08-25 11:48:14 +0800

    bfs

--- src/graph/graph.test.ts
+++ src/graph/graph.test.ts
@@ -1,9 +1,25 @@
-import Graph from "./graph";
+import Graph, { bfs, BFS } from "./graph";
 
 let graph;
 
 beforeAll(() => {
   graph = new Graph();
+  const myVertices = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+  for (let i = 0; i < myVertices.length; i++) {
+    graph.addVertex(myVertices[i]);
+  }
+  graph.addEdge("A", "B");
+
+  graph.addEdge("A", "C");
+  graph.addEdge("A", "D");
+  graph.addEdge("C", "D");
+  graph.addEdge("C", "G");
+
+  graph.addEdge("D", "G");
+  graph.addEdge("D", "H");
+  graph.addEdge("B", "E");
+  graph.addEdge("B", "F");
+  graph.addEdge("E", "I");
 });
 
 afterAll(() => {
@@ -14,53 +30,20 @@ const printNode = (val) => {
 };
 describe("Graph", () => {
   it("add: 添加节点叶线段", () => {
-    const myVertices = ["A", "B", "C", "D", "E", "F"];
-    for (let i = 0; i < myVertices.length; i++) {
-      graph.addVertex(myVertices[i]);
-    }
-    graph.addEdge("A", "C");
-    graph.addEdge("A", "D");
-    graph.addEdge("B", "D");
-    graph.addEdge("B", "E");
-    graph.addEdge("C", "F");
-    graph.addEdge("F", "E");
+    // console.log(graph.toString());
   });
-  it("bfs", () => {
-    graph.bfs(graph.getVertics()[0], printNode);
-  });
+  // it("bfs", () => {
+  //   bfs(graph, "A", printNode);
+  // });
   it("BFS", () => {
-    const res = graph.BFS(graph.getVertics()[0]);
+    const res = BFS(graph, "A");
     console.log(res);
   });
-  it("dfs", () => {
-    graph.dfs(printNode);
-  });
-  it("DFS", () => {
-    const res = graph.DFS();
-    console.log(res);
-  });
-  // it("dijkstra", () => {
-  //   var graph_martix = [
-  //     [0, 2, 4, 0, 0, 0],
-  //     [0, 0, 1, 4, 2, 0],
-  //     [0, 0, 0, 0, 3, 0],
-  //     [0, 0, 0, 0, 0, 2],
-  //     [0, 0, 0, 3, 0, 2],
-  //     [0, 0, 0, 0, 0, 0],
-  //   ];
-  //   const res = graph.dijkstra(graph_martix, 0);
+  // it("dfs", () => {
+  //   graph.dfs(printNode);
+  // });
+  // it("DFS", () => {
+  //   const res = graph.DFS();
   //   console.log(res);
   // });
-  it("floydWarshall", () => {
-    var graph_martix = [
-      [0, 2, 4, 0, 0, 0],
-      [0, 0, 1, 4, 2, 0],
-      [0, 0, 0, 0, 3, 0],
-      [0, 0, 0, 0, 0, 2],
-      [0, 0, 0, 3, 0, 2],
-      [0, 0, 0, 0, 0, 0],
-    ];
-    const res = graph.floydWarshall(graph_martix);
-    console.log(res);
-  });
 });
--- src/graph/graph.ts
+++ src/graph/graph.ts
@@ -1,6 +1,11 @@
 import Dictionary from "../dictionary/dictionary";
 import Queue from "../queue/queue";
 // import Stack from "../stack/stack";
+const Colors = {
+  WHITE: 0,
+  GREY: 1,
+  BLACK: 2,
+};
 class Graph {
   private vertices;
   private adjList;
@@ -37,129 +42,12 @@ class Graph {
   getAdjList() {
     return this.adjList;
   }
-  initializeColor() {
-    let color = [];
-    for (let i = 0; i < this.vertices.length; i++) {
-      color[this.vertices[i]] = "white"; // 1
+  initializeColor(vertices) {
+    let color = {};
+    for (let i = 0; i < vertices.length; i++) {
+      color[vertices[i]] = Colors.WHITE; // 1
     }
     return color;
-  }
-  bfs(v, callback) {
-    let color = this.initializeColor(); //2
-    let queue = new Queue(); //3
-    queue.enqueue(v); //4
-    while (!queue.isEmpty()) {
-      //5
-      let u = queue.dequeue(); //6
-      let neighbors = this.adjList.get(u); //7
-      color[u] = "grey"; //8
-      for (let i = 0; i < neighbors.length; i++) {
-        //9
-        let w = neighbors[i]; //10
-        if (color[w] === "white") {
-          //11
-          color[w] = "grey"; //12
-          queue.enqueue(w); //13
-        }
-      }
-      color[u] = "black"; //14
-      if (callback) {
-        //15
-        callback(u);
-      }
-    }
-  }
-  BFS(v) {
-    let color = this.initializeColor();
-    let queue = new Queue();
-    let distances = []; //1 距离
-    let pred = []; //2 前溯点
-    queue.enqueue(v);
-    for (var i = 0; i < this.vertices.length; i++) {
-      //{3}
-      distances[this.vertices[i]] = 0; //4
-      pred[this.vertices[i]] = null; //5
-    }
-
-    while (!queue.isEmpty()) {
-      let u = queue.dequeue(),
-        neighbors = this.adjList.get(u);
-      color[u] = "grey";
-      for (i = 0; i < neighbors.length; i++) {
-        var w = neighbors[i];
-        if (color[w] === "white") {
-          color[w] = "grey";
-          distances[w] = distances[u] + 1; //6
-          pred[w] = u; //7
-          queue.enqueue(w);
-        }
-      }
-      color[u] = "black";
-    }
-    return {
-      //{8}
-      distances: distances,
-      predecessors: pred,
-    };
-  }
-  dfs(callback) {
-    let color = this.initializeColor();
-    for (let i = 0; i < this.vertices.length; i++) {
-      if (color[this.vertices[i]] === "white") {
-        this.dfsVisit(this.vertices[i], color, callback);
-      }
-    }
-  }
-  dfsVisit(u, color, callback) {
-    color[u] = "grey";
-    if (callback) {
-      callback(u);
-    }
-    let neighbors = this.adjList.get(u);
-    for (let i = 0; i < neighbors.length; i++) {
-      let w = neighbors[i];
-      if (color[w] === "white") {
-        this.dfsVisit(w, color, callback);
-      }
-    }
-    color[u] = "black";
-  }
-  DFS() {
-    var color = this.initializeColor(), //{2}
-      d = [],
-      p = [],
-      f = [];
-    for (var i = 0; i < this.vertices.length; i++) {
-      //{3}
-      const vertex = this.vertices[i];
-      f[vertex] = 0;
-      d[vertex] = 0;
-      p[vertex] = null;
-    }
-    for (i = 0; i < this.vertices.length; i++) {
-      const vertex = this.vertices[i];
-      if (color[vertex] === "white") {
-        this.DFSVisit(vertex, color, d, f, p);
-      }
-    }
-    // 4
-    return { discovery: d, finished: f, predecessors: p };
-  }
-  DFSVisit(u, color, d, f, p) {
-    console.log("discovered " + u);
-    color[u] = "grey";
-    d[u] = ++this.time; //{5}
-    var neighbors = this.adjList.get(u);
-    for (var i = 0; i < neighbors.length; i++) {
-      var w = neighbors[i];
-      if (color[w] === "white") {
-        p[w] = u; //6
-        this.DFSVisit(w, color, d, f, p);
-      }
-    }
-    color[u] = "black";
-    f[u] = ++this.time; //{7}
-    console.log("explored " + u);
   }
   toString() {
     let s = "";
@@ -173,74 +61,71 @@ class Graph {
     }
     return s;
   }
-
-  // 最短路径算法
-  // dijkstra
-  dijkstra(src, point) {
-    // src => 邻接矩阵 -> 二维数组
-    const minDistance = (dist, visited) => {
-      var min = Infinity,
-        minIndex = -1;
-      for (var v = 0; v < dist.length; v++) {
-        if (visited[v] == false && dist[v] <= min) {
-          min = dist[v];
-          minIndex = v;
-        }
-      }
-      return minIndex;
-    };
-    var dist = [],
-      visited = [],
-      length = src.length;
-    for (var i = 0; i < length; i++) {
-      //{1}
-      dist[i] = Infinity;
-    }
-    dist[point] = 0; //{2}
-    for (var i = 0; i < length - 1; i++) {
-      //{3}
-      var u = minDistance(dist, visited);
-      visited[u] = true; //{5}
-      for (var v = 0; v < length; v++) {
-        console.log(u, v);
-        if (
-          !visited[v] &&
-          src[u] &&
-          src[u][v] != 0 &&
-          dist[u] != Infinity &&
-          dist[u] + src[u][v] < dist[v]
-        ) {
-          //{6}
-          dist[v] = dist[u] + src[u][v]; //{7} }
-        }
-      }
-      return dist;
-    }
-  }
-  // floydWarshall
-  floydWarshall(graph) {
-    let dist = [],
-      length = graph.length,
-      i,
-      j,
-      k;
-    for (i = 0; i < length; i++) {
-      dist[i] = [];
-      for (j = 0; j < length; j++) {
-        dist[i][j] = graph[i][j];
-      }
-    }
-    for (k = 0; k < length; k++) {
-      for (i = 0; i < length; i++) {
-        for (j = 0; j < length; j++) {
-          if (dist[i][k] + dist[k][j] < dist[i][j]) {
-            dist[i][j] = dist[i][k] + dist[k][j];
-          }
-        }
-      }
-    }
-    return dist;
-  }
 }
+
+export const bfs = (graph, startVertex, callback) => {
+  const vertices = graph.getVertics();
+  const adjList = graph.getAdjList();
+  const colors = graph.initializeColor(vertices);
+  const queue = new Queue();
+  queue.enqueue(startVertex);
+
+  while (!queue.isEmpty()) {
+    const u = queue.dequeue();
+    colors[u] = Colors.GREY;
+    const neighbors = adjList.get(u);
+    for (let i = 0; i < neighbors.length; i++) {
+      const w = neighbors[i];
+      if (colors[w] === Colors.WHITE) {
+        colors[w] = Colors.GREY;
+        queue.enqueue(w);
+      }
+    }
+    colors[u] = Colors.BLACK;
+    if (callback) {
+      callback(u);
+    }
+  }
+};
+
+export const BFS = (graph, startVertex, callback) => {
+  const vertices = graph.getVertics();
+  const adjList = graph.getAdjList();
+  const colors = graph.initializeColor(vertices);
+  const queue = new Queue();
+
+  const distances = {};
+  const predecessors = {};
+
+  queue.enqueue(startVertex);
+
+  for (let i = 0; i < vertices.length; i++) {
+    distances[vertices[i]] = 0;
+    predecessors[vertices[i]] = null;
+  }
+  while (!queue.isEmpty()) {
+    const u = queue.dequeue();
+    colors[u] = Colors.GREY;
+    const neighbors = adjList.get(u);
+    for (let i = 0; i < neighbors.length; i++) {
+      const w = neighbors[i];
+      if (colors[w] === Colors.WHITE) {
+        colors[w] = Colors.GREY;
+        distances[w] = distances[u] + 1;
+        predecessors[w] = u;
+        console.log(w);
+        queue.enqueue(w);
+      }
+    }
+    colors[u] = Colors.BLACK;
+    if (callback) {
+      callback(u);
+    }
+    return {
+      distances,
+      predecessors,
+    };
+  }
+};
 
 export default Graph;
